750e60ec89acbbcd8b4c142ba83fadf03116a097
Gabriel Maia <gabrielmaialva33@gmail.com>
2026-09-05 21:41:49 -0300

    vk overlay: add read access mask to render pass color attachment dependency

--- OptiScaler/menu/menu_overlay_vk.cpp
+++ OptiScaler/menu/menu_overlay_vk.cpp
@@ -469,7 +469,7 @@ static void CreateVulkanObjects(VkDevice device, VkPhysicalDevice pd, VkInstance
         dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
         dependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
         dependency.srcAccessMask = 0;
-        dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+        dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT | VK_ACCESS_COLOR_ATTACHMENT_READ_BIT;
 
         VkRenderPassCreateInfo render_pass_info = {};
         render_pass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
